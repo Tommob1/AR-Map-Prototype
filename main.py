29d05868b2b3bc9d692b7e74944fcdf1f91d4a66
Brayden Tomlinson <humortronic@gmail.com>
2024-08-15 15:11:42 +0930

    Added changes to code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import cv2
 import pygame
 import numpy as np
 import asyncio
 import websockets
 
 pygame.init()
-cap = cv2.VideoCapture(0)
 
 window_size = (640, 480)
 screen = pygame.display.set_mode(window_size)
@@ -14,7 +12,7 @@ heading = 0
 
 async def receive_heading():
     global heading
-    async with websockets.connect("ws://localhost:8765") as websocket:
+    async with websockets.connect("ws://172.20.10.10:8765") as websocket:
         while True:
             message = await websocket.recv()
             heading = float(message.split(":")[1].strip())
@@ -30,36 +28,16 @@ def draw_compass(screen, heading):
 
 async def main():
     global heading
-
-    # Start receiving heading in the background
     asyncio.create_task(receive_heading())
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        scale_factor = 0.5  # Adjust this value as needed
-        frame = cv2.resize(frame, None, fx=scale_factor, fy=scale_factor)
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = np.rot90(frame)
-        frame_surface = pygame.surfarray.make_surface(frame)
-
-        frame_rect = frame_surface.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
-
         screen.fill((255, 255, 255))
-        screen.blit(frame_surface, frame_rect.topleft)
-
         draw_compass(screen, heading)
-
         pygame.display.flip()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if pygame.key.get_pressed()[pygame.K_q]:
             break
+        await asyncio.sleep(0.01)
 
-    cap.release()
-    cv2.destroyAllWindows()
     pygame.quit()
-
 asyncio.run(main())
